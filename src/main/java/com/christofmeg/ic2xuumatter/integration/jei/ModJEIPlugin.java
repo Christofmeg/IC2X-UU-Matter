c986679fa83994c49fddb8de9f83ad4e62f2941a
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2023-04-05 18:11:20 +0200

    wip

--- src/main/java/com/christofmeg/ic2xuumatter/integration/jei/ModJEIPlugin.java
+++ src/main/java/com/christofmeg/ic2xuumatter/integration/jei/ModJEIPlugin.java
@@ -1,7 +1,10 @@
 package com.christofmeg.ic2xuumatter.integration.jei;
 
+import java.awt.Rectangle;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import javax.annotation.Nonnull;
 
@@ -9,7 +12,6 @@ import com.christofmeg.ic2xuumatter.integration.jei.category.MassFabricatorCateg
 import com.christofmeg.ic2xuumatter.integration.jei.category.ReplicatorCategory;
 import com.christofmeg.ic2xuumatter.utils.Utils;
 
-import ic2.core.block.machine.container.ContainerReplicator;
 import ic2.core.block.machine.gui.GuiMatter;
 import ic2.core.block.machine.gui.GuiReplicator;
 import ic2.core.item.type.CellType;
@@ -22,7 +24,10 @@ import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.gui.IAdvancedGuiHandler;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.relauncher.Side;
@@ -38,6 +43,7 @@ public class ModJEIPlugin implements IModPlugin {
         registration.addRecipeCatalyst(BlockName.te.getItemStack(TeBlock.matter_generator), MassFabricatorCategory.UID);
 
         registration.addRecipeCatalyst(BlockName.te.getItemStack(TeBlock.replicator), ReplicatorCategory.UID);
+        registration.addRecipeCatalyst(BlockName.te.getItemStack(TeBlock.pattern_storage), ReplicatorCategory.UID);
 
         registration.addRecipes(Arrays.asList(
                 new MassFabricatorCategory.MatterFabricatorRecipe(null,
@@ -53,34 +59,31 @@ public class ModJEIPlugin implements IModPlugin {
                         Utils.getCellFromFluid(FluidName.uu_matter.getName()))),
                 MassFabricatorCategory.UID);
 
-        registration
-                .addRecipes(
-                        Collections.singletonList(
-                                new ReplicatorCategory.ReplicatorRecipe(ItemName.cell.getItemStack(CellType.empty),
-                                        Utils.getCellFromFluid(FluidName.uu_matter.getName()),
-                                        new FluidStack(FluidName.uu_matter.getInstance(), 1000))),
-                        ReplicatorCategory.UID);
+        registration.addRecipes(Collections.singletonList(new ReplicatorCategory.ReplicatorRecipe(
+                Utils.getCellFromFluid(FluidName.uu_matter.getName()), ItemName.cell.getItemStack(CellType.empty),
+                new FluidStack(FluidName.uu_matter.getInstance(), 1000))), ReplicatorCategory.UID);
 
         registration.addRecipeClickArea(GuiMatter.class, 117, 41, 21, 16, MassFabricatorCategory.UID);
-        registration.addRecipeClickArea(GuiReplicator.class, 78, 14, 20, 54, ReplicatorCategory.UID);
+        registration.addRecipeClickArea(GuiReplicator.class, 12, 45, 13, 24, ReplicatorCategory.UID);
+
+//        registration.addAdvancedGuiHandlers(new GlobalReplicatorHandler());
 
         /*
          * int mrecipeSlotStart = 0; int mrecipeSlotCount = 2; int minventorySlotStart =
          * 3; int minventorySlotCount = 39;
          *
-         * // TODO MATTER Fabricator Transfer handler matter Show energy required to put
-         * uu registration.getRecipeTransferRegistry().addRecipeTransferHandler(
+         * // TODO MATTER Fabricator Transfer handler matter
+         * registration.getRecipeTransferRegistry().addRecipeTransferHandler(
          * ContainerMatter.class, MassFabricatorCategory.UID, mrecipeSlotStart,
          * mrecipeSlotCount, minventorySlotStart, minventorySlotCount);
+         *
+         *
+         * int recipeSlotStart = 1; int recipeSlotCount = 2; int inventorySlotStart = 4;
+         * int inventorySlotCount = 36;
+         * registration.getRecipeTransferRegistry().addRecipeTransferHandler(
+         * ContainerReplicator.class, ReplicatorCategory.UID, recipeSlotStart,
+         * recipeSlotCount, inventorySlotStart, inventorySlotCount);
          */
-
-        int recipeSlotStart = 1;
-        int recipeSlotCount = 2;
-        int inventorySlotStart = 4;
-        int inventorySlotCount = 36;
-        registration.getRecipeTransferRegistry().addRecipeTransferHandler(ContainerReplicator.class,
-                ReplicatorCategory.UID, recipeSlotStart, recipeSlotCount, inventorySlotStart, inventorySlotCount);
-
     }
 
     @Override
@@ -88,6 +91,28 @@ public class ModJEIPlugin implements IModPlugin {
         IGuiHelper helper = registration.getJeiHelpers().getGuiHelper();
 
         registration.addRecipeCategories(new MassFabricatorCategory(helper), new ReplicatorCategory(helper));
+    }
+
+    public static class GlobalReplicatorHandler implements IAdvancedGuiHandler<GuiContainer> {
+
+        @Override
+        public List<Rectangle> getGuiExtraAreas(GuiContainer guiContainer) {
+            int x = guiContainer.getGuiLeft() - 124;
+            int y = guiContainer.getGuiTop();
+            Minecraft minecraft = guiContainer.mc;
+            if (minecraft == null) {
+                return Collections.emptyList();
+            }
+            List<Rectangle> areas = new ArrayList<>();
+            areas.add(new Rectangle(x, y, 140, 32));
+            return areas;
+
+        }
+
+        @Override
+        public Class<GuiContainer> getGuiContainerClass() {
+            return GuiContainer.class;
+        }
     }
 
 }
--- src/main/java/com/christofmeg/ic2xuumatter/integration/jei/category/ReplicatorCategory.java
+++ src/main/java/com/christofmeg/ic2xuumatter/integration/jei/category/ReplicatorCategory.java
@@ -6,6 +6,8 @@ import ic2.core.ref.BlockName;
 import ic2.core.ref.TeBlock;
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.gui.IDrawable;
+import mezz.jei.api.gui.IDrawableAnimated.StartDirection;
+import mezz.jei.api.gui.IDrawableBuilder;
 import mezz.jei.api.gui.IDrawableStatic;
 import mezz.jei.api.gui.IGuiFluidStackGroup;
 import mezz.jei.api.gui.IGuiItemStackGroup;
@@ -26,14 +28,66 @@ public class ReplicatorCategory implements IRecipeCategory<ReplicatorCategory.Re
     public static String UID = "ic2xuumatter.replicator";
 
     IDrawable background;
+    IDrawable energy;
+    IDrawableBuilder patternStorage;
     protected IDrawableStatic tankOverlay;
+
+    IDrawableBuilder patternGuiBackground1;
+    IDrawableBuilder patternGuiBackground2;
+    IDrawableBuilder patternGuiBackground3;
+    IDrawableBuilder patternGuiBackground4;
+    IDrawableBuilder patternGuiBackground5;
+    IDrawableBuilder patternGuiBackground6;
+
+    IDrawableBuilder patternGuiCorner;
+    IDrawableBuilder patternGuiBorderFix;
+    IDrawableBuilder patternGuiCornerPixel;
+
+    IDrawableBuilder patternGuiRecipeBackground1;
+    IDrawableBuilder patternGuiRecipeBackground2;
+    IDrawableBuilder patternGuiRecipeBackground3;
+    IDrawableBuilder patternGuiRecipeBackground4;
+    IDrawableBuilder patternGuiRecipeBackground5;
+    IDrawableBuilder patternGuiRecipeBackground6;
 
     public static final ResourceLocation replicatorTexture = new ResourceLocation("ic2",
             "textures/gui/guireplicator.png");
+    public static final ResourceLocation patternStorageTexture = new ResourceLocation("ic2",
+            "textures/gui/guipatternstorage.png");
+    public static final ResourceLocation jeiGuiBackground = new ResourceLocation("jei",
+            "textures/gui/gui_background.png");
+    public static final ResourceLocation jeiCatalystTab = new ResourceLocation("jei", "textures/gui/catalyst_tab.png");
+    public static final ResourceLocation jeiRecipeBackground = new ResourceLocation("jei",
+            "textures/gui/single_recipe_background.png");
 
     public ReplicatorCategory(IGuiHelper helper) {
-        background = helper.createDrawable(replicatorTexture, 7, 6, 163, 93);
-        tankOverlay = helper.createDrawable(replicatorTexture, 31, 34, 16, 60);
+        background = helper.createDrawable(replicatorTexture, 7, 7, 162, 93);
+        energy = helper.drawableBuilder(replicatorTexture, 176, 0, 14, 15).buildAnimated(300, StartDirection.TOP, true);
+        tankOverlay = helper.createDrawable(replicatorTexture, 48 + 64 * 2, 193, 16, 60);
+        patternStorage = helper.drawableBuilder(patternStorageTexture, 7, 19, 162, 62);
+
+        patternGuiBackground1 = helper.drawableBuilder(jeiGuiBackground, 0, 28, 60, 36).setTextureSize(64, 64);
+        patternGuiBackground2 = helper.drawableBuilder(jeiGuiBackground, 0, 0, 60, 45).setTextureSize(64, 64);
+
+        patternGuiBackground3 = helper.drawableBuilder(jeiGuiBackground, 4, 28, 56, 36).setTextureSize(64, 64);
+        patternGuiBackground4 = helper.drawableBuilder(jeiGuiBackground, 4, 0, 56, 45).setTextureSize(64, 64);
+
+        patternGuiBackground5 = helper.drawableBuilder(jeiGuiBackground, 4, 28, 11, 36).setTextureSize(64, 64);
+        patternGuiBackground6 = helper.drawableBuilder(jeiGuiBackground, 4, 0, 11, 45).setTextureSize(64, 64);
+
+        patternGuiCorner = helper.drawableBuilder(jeiCatalystTab, 22, 25, 3, 3).setTextureSize(28, 28);
+        patternGuiBorderFix = helper.drawableBuilder(jeiCatalystTab, 0, 4, 3, 1).setTextureSize(28, 28);
+        patternGuiCornerPixel = helper.drawableBuilder(jeiCatalystTab, 2, 25, 1, 1).setTextureSize(28, 28);
+
+        patternGuiRecipeBackground1 = helper.drawableBuilder(jeiRecipeBackground, 0, 56, 62, 8).setTextureSize(64, 64);
+        patternGuiRecipeBackground2 = helper.drawableBuilder(jeiRecipeBackground, 0, 0, 62, 62).setTextureSize(64, 64);
+
+        patternGuiRecipeBackground3 = helper.drawableBuilder(jeiRecipeBackground, 2, 56, 60, 8).setTextureSize(64, 64);
+        patternGuiRecipeBackground4 = helper.drawableBuilder(jeiRecipeBackground, 2, 0, 60, 62).setTextureSize(64, 64);
+
+        patternGuiRecipeBackground5 = helper.drawableBuilder(jeiRecipeBackground, 16, 56, 48, 8).setTextureSize(64, 64);
+        patternGuiRecipeBackground6 = helper.drawableBuilder(jeiRecipeBackground, 16, 0, 48, 62).setTextureSize(64, 64);
+
     }
 
     @Override
@@ -64,17 +118,49 @@ public class ReplicatorCategory implements IRecipeCategory<ReplicatorCategory.Re
      */
 
     @Override
+    public void drawExtras(Minecraft minecraft) {
+        energy.draw(minecraft, 126, 76);
+        /*
+         * patternGuiBackground1.build().draw(minecraft, -194, 35);
+         * patternGuiBackground2.build().draw(minecraft, -194, -9);
+         *
+         * patternGuiBackground3.build().draw(minecraft, -138, 35);
+         * patternGuiBackground4.build().draw(minecraft, -138, -9);
+         *
+         * patternGuiBackground3.build().draw(minecraft, -82, 35);
+         * patternGuiBackground4.build().draw(minecraft, -82, -9);
+         *
+         * patternGuiBackground5.build().draw(minecraft, -26, 35);
+         * patternGuiBackground6.build().draw(minecraft, -26, -9);
+         *
+         * patternGuiCorner.build().draw(minecraft, -18, 68);
+         * patternGuiBorderFix.build().draw(minecraft, -18, -9);
+         * patternGuiCornerPixel.build().draw(minecraft, -16, -7);
+         *
+         * patternGuiRecipeBackground1.build().draw(minecraft, -189, 58);
+         * patternGuiRecipeBackground2.build().draw(minecraft, -189, -4);
+         *
+         * patternGuiRecipeBackground3.build().draw(minecraft, -127, 58);
+         * patternGuiRecipeBackground4.build().draw(minecraft, -127, -4);
+         *
+         * patternGuiRecipeBackground5.build().draw(minecraft, -67, 58);
+         * patternGuiRecipeBackground6.build().draw(minecraft, -67, -4);
+         *
+         * patternStorage.build().draw(minecraft, -185, 0);
+         */
+    }
+
+    @Override
     public void setRecipe(IRecipeLayout recipeLayout, ReplicatorRecipe recipeWrapper, IIngredients ingredients) {
         IGuiItemStackGroup guiItemStacks = recipeLayout.getItemStacks();
         IGuiFluidStackGroup guiFluidStacks = recipeLayout.getFluidStacks();
 
         List<List<FluidStack>> fluidOutput = ingredients.getOutputs(VanillaTypes.FLUID);
-        List<List<FluidStack>> fluidInput = ingredients.getInputs(VanillaTypes.FLUID);
 
-        guiFluidStacks.init(0, false, 24, 28, 12, 47, 8000, true, tankOverlay); // uu matter
+        guiFluidStacks.init(0, false, 24, 27, 12, 47, 16000, true, tankOverlay); // uu matter
 
-        guiItemStacks.init(0, true, 0, 65); // filled cell
-        guiItemStacks.init(1, false, 0, 20); // empty cell
+        guiItemStacks.init(0, false, 0, 64); // empty cell
+        guiItemStacks.init(1, true, 0, 19); // filled cell
         guiFluidStacks.set(0, fluidOutput.get(0)); // uu matter
 
         guiItemStacks.set(ingredients);
